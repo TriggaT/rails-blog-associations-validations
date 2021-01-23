ad7eea3d543eca868e846098c50b92c0d4a8604c
Trayshawn <traytwebb14@yahoo.com>
2021-01-22 23:02:46 -0600

    commit tree e51eb870eb6aafe88a20d854617a5e5482cc0d49

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,20 @@
 class Post < ActiveRecord::Base
+    belongs_to :user
+    has_many :post_tags
+    has_many :tags, through: :post_tags
+
+    validates :name, :content, presence: true 
+
+    accepts_nested_attributes_for :tags
+
+    def tags_attributes=(tag_attributes)
+        tag_attributes.value.each do |tag_attribute|
+           if tag_attribute["name"].present?
+               tag = Tag.find_or_create(tag_attribute)
+               self.tags << Tag.new(tag_attribute)
+           end 
+       end 
+   end 
+
   
 end
